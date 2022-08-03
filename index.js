eb0df39e6f6ccc0774cefbdfff8928a5e24c9e38
rikiarizona <rikiarizona@gmail.com>
2022-08-03 10:05:09 +0700

    TUGAS 18-1 JAVASCRIPT
    saya kirimkan tugas niomic

---
+++ index.js
@@ -0,0 +1,5 @@
+function duaKata() {
+    var kata = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.";
+    console.log(kata.slice(8, 24));
+}
+duaKata();
